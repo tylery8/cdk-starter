6d78a888763adf102412530c164acd8dc5a474e1
tylery8 <tylertfc99@gmail.com>
2021-10-03 00:32:32 -0500

    cleanup

--- resources/publish/lambda_function.py
+++ resources/publish/lambda_function.py
@@ -2,17 +2,18 @@ from random import randint
 import boto3
 import os
 
+
 def lambda_handler(event, context):
-    randomInt = randint(0, 100)
+    random_int = randint(0, 100)
 
     client = boto3.client('sqs')
 
     resp = client.send_message(
         QueueUrl=os.environ['QUEUE_URL'],
-        MessageBody=str(randomInt)
+        MessageBody=str(random_int)
     )
 
     return {
         'statusCode': 200,
-        'body': 'Successfully pushed message ' + str(randomInt) + '!!'
+        'body': 'Successfully pushed message ' + str(random_int) + '!!'
     }
--- resources/subscribe/lambda_function.py
+++ resources/subscribe/lambda_function.py
@@ -1,17 +1,18 @@
 import boto3
 import os
 
+
 def lambda_handler(event, context):
     for record in event['Records']:
-        id = record['body']
-        print(id)
+        _id = record['body']
+        print(_id)
 
         client = boto3.client('dynamodb')
 
         resp = client.put_item(
             TableName=os.environ['TABLE_NAME'],
             Item={
-                'id': {'N': id }
+                'id': {'N': _id}
             }
         )
     
